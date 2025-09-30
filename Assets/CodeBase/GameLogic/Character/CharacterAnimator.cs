759842b94a33a4f98cd95abde91f8e4599863253
Sergei Anisimov <s.anisimov.it@gmail.com>
2025-09-30 16:53:14 +0200

    Animation

--- Assets/CodeBase/GameLogic/Character/CharacterAnimator.cs
+++ Assets/CodeBase/GameLogic/Character/CharacterAnimator.cs
@@ -1,9 +1,12 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace CodeBase.GameLogic.Character
 {
   public class CharacterAnimator : MonoBehaviour
   {
+    [SerializeField, BoxGroup("ANIMATOR SETUP")] private Animator _animator;
+    
     
   }
 }
